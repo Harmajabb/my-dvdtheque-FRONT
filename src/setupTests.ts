66ba4510e3cfdedc39a7651ce2daa18a3326588e
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2026-02-13 14:29:09 +0100

    fix - setupTest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,27 @@
+global.IntersectionObserver = class IntersectionObserver {
+  readonly root = null;
+  readonly rootMargin = "0px";
+  readonly thresholds = [0];
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords() {
+    return [];
+  }
+} as unknown as typeof globalThis.IntersectionObserver;
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
+
 import "@testing-library/jest-dom";
